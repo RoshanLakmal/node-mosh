906f5393ddc22ab299e77468f6d6f9f7a2f18128
Roshan <roshanrox911@gmail.com>
2019-09-17 22:28:47 +1000

    added basic id serach form array & sending 404 error

--- node-mosh/4-1-First Web Server/index.js
+++ node-mosh/4-1-First Web Server/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
+});
+
+/* 
+  IN - http://localhost:3000/api/posts/1
+  OUT - {
+          "id": 1,
+          "name": "course1"
+        }
+
+  IN - http://localhost:3000/api/courses/10
+  OUT - The course with the given ID was not found
+*/
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course with the given ID was not found');
+  res.send(course);
 });
 
 /* 
