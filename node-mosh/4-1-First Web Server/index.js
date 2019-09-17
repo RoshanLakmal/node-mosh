3c4655087682b528d6ff738e96813a6ebd26264d
Roshan <roshanrox911@gmail.com>
2019-09-17 23:55:12 +1000

    added joi with input validation

--- node-mosh/4-1-First Web Server/index.js
+++ node-mosh/4-1-First Web Server/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -72,8 +73,81 @@ app.get('/api/posts/:year/:month', (req, res) => {
           "id": 4,
           "name": "new course"
         }
+
+  IN - http://localhost:3000/api/courses 
+  Then select the body as a request raw. In the drop text drop down select JSON(applicaiton/json)
+  {
+    "name": "a"
+  }
+  OUT - Name is required and shoud be minimum 3 character long 
+      - With 400 Bad request
 */
 app.post('/api/courses', (req, res) => {
+  /*  This error handling is very bad use joi instead*/
+  // if (!req.body.name || req.body.name.length < 3) {
+  //   //400 Bad Request
+  //   res
+  //     .status(400)
+  //     .send('Name is required and shoud be minimum 3 character long');
+  // }
+
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+  /*
+    IN - http://localhost:3000/api/courses 
+    Then select the body as a request raw. In the drop text drop down select JSON(applicaiton/json)
+    CORRECT INPUT
+    {
+      "name": "name"
+    }
+    OUT IN CONSOLE FOR result
+      { error: null,
+        value: { name: 'name' },
+        then: [Function: then],
+        catch: [Function: catch] 
+      }
+
+
+      IN - http://localhost:3000/api/courses 
+    Then select the body as a request raw. In the drop text drop down select JSON(applicaiton/json)
+    INCORRECT INPUT
+    {
+      "name": "a"
+    }
+    OUT IN CONSOLE FOR result
+      { error: { ValidationError: child "name" fails because ["name" length must be at least 3 characters long].........,
+        isJoi: true,
+        name: 'ValidationError',
+        details: [ [Object] ],
+        _object: { name: 'a' },
+        annotate: [Function] },
+        value: { name: 'a' },
+        then: [Function: then],
+        catch: [Function: catch] 
+      }
+  */
+  console.log(result);
+
+  if (result.error) {
+    //400 Bad Request
+    //res.status(400).send(result.error); /* Too complex we can simplyfy*/
+    /* 
+      In - {
+            "name": "a"
+           }
+      Out - "name" length must be at least 3 characters long
+
+      In - {}
+      Out - "name" is required
+    */
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
   const course = {
     id: courses.length + 1,
     name: req.body.name
