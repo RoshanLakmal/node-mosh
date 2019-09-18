45de398c2d4921a2cbdf69d40cc0475b59e1d942
Roshan <roshanrox911@gmail.com>
2019-09-19 00:25:42 +1000

    debug module

--- node-mosh/5-1-Middleware/index.js
+++ node-mosh/5-1-Middleware/index.js
@@ -1,4 +1,6 @@
 const config = require('config');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
@@ -11,6 +13,21 @@ app.use(express.urlencoded({ extended: true })); //key=value&key=value
 app.use(express.static('public')); //store static assests (http://localhost:3000/readme.txt - static content server from the root of the site)
 app.use(helmet());
 
+//cmd - exprt NODE_ENV = production
+//      export NODE_ENV=development
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  // console.log('Morgan enabled...');
+  // cmd      - export DEBUG=app:startup
+  // cmd      - export DEBUG=app:startup,app:db
+  // cmd      - export DEBUG=app:*
+  // Reseting - export DEBUG=
+  startupDebugger('Morgan enabled...');
+}
+
+//Db Work
+dbDebugger('Connected to the database...!');
+
 // Configuration change accordingly to exprt NODE_ENV varialbe
 // config/default.json, development.json, production.json
 console.log('Application Name: ' + config.get('name'));
@@ -19,13 +36,6 @@ console.log('Mail Server: ' + config.get('mail.host'));
 //adding sensitive data cmd - export app_password=1234
 // config/custom-enviornment-variables.json
 console.log('Mail Password: ' + config.get('mail.password'));
-
-//cmd - exprt NODE_ENV = production
-//      export NODE_ENV=development
-if (app.get('env') === 'development') {
-  app.use(morgan('tiny'));
-  console.log('Morgan enabled...');
-}
 
 app.use(logger); //Middleware function (called in squence - logger)
 
