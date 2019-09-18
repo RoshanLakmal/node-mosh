75195d792b519b8d4630cb9c6e6434fce982e789
Roshan <roshanrox911@gmail.com>
2019-09-19 01:02:42 +1000

    structure

--- node-mosh/5-1-Middleware/index.js
+++ node-mosh/5-1-Middleware/index.js
@@ -4,7 +4,9 @@ const dbDebugger = require('debug')('app:db');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
-const logger = require('./logger');
+const logger = require('./middleware/logger');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
@@ -16,7 +18,8 @@ app.use(express.json()); // req.body
 app.use(express.urlencoded({ extended: true })); //key=value&key=value
 app.use(express.static('public')); //store static assests (http://localhost:3000/readme.txt - static content server from the root of the site)
 app.use(helmet());
-
+app.use('/api/courses', courses);
+app.use('/', home);
 //cmd - exprt NODE_ENV = production
 //      export NODE_ENV=development
 if (app.get('env') === 'development') {
@@ -45,43 +48,5 @@ console.log('Mail Password: ' + config.get('mail.password'));
 
 app.use(logger); //Middleware function (called in squence - logger)
 
-const courses = [
-  { id: 1, name: 'course1' },
-  { id: 2, name: 'course2' },
-  { id: 3, name: 'course3' }
-];
-
-app.get('/', (req, res) => {
-  // res.send('Hello World');
-  res.render('index', { title: 'My Express App', message: 'Hello' });
-});
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-function validateCourse(course) {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-
-  return Joi.validate(course, schema);
-}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
